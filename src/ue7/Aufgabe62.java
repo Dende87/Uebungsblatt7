c560bbdbbe21a637ed819e3cc963eeea13faacc0
Dende <dende76@gmx.de>
2015-12-24 16:41:48 +0100

    Adding quest

---
+++ src/ue7/Aufgabe62.java
@@ -0,0 +1,25 @@
+package ue7;
+
+public class Aufgabe62 {
+	
+	public static void main(String[] a){
+		
+		int[][] matrix = {{1,2,3},{1,2,3},{1,2,3,4,2}};
+		anzahlElemente(matrix);
+	}
+
+	private static void anzahlElemente(int[][] matrix) {
+
+		int counter = 0;
+		for(int[] anzahl: matrix){
+			
+			for(int i: anzahl){
+				counter++;
+			}
+			
+		}
+		System.out.print(counter);
+		
+	}
+
+}
